e266366679f52de14f82f4fc5062c0c344c9b973
Polaroid339 <victor339335@gmail.com>
2025-03-29 09:41:21 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    // Entrada de dados
+    String inputSenai = JOptionPane.showInputDialog("Insira um numero inteiro");
+    int input = Integer.parseInt(inputSenai);
+    }
+}
